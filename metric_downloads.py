334bfd00bf103517d0df327efa588e51c4149e3c
Yamer Yusuf <y2yusuf@uwaterloo.ca>
2021-05-16 19:39:43 -0400

    1. Added API for Technical Indicator Calculations 2. Added plots of Technical Indicators

--- metric_downloads.py
+++ metric_downloads.py
@@ -13,9 +13,14 @@ import pandas as pd
 import requests
 import datetime
 import json
+from ta import add_all_ta_features
+from ta.utils import dropna
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.dates import DateFormatter
 
 uri = 'https://www.alphavantage.co/query?'
-
+plt.close("all")
 
 def process_times(startmonth, startyear, endmonth, endyear):
     now = datetime.date(datetime.datetime.now().year, datetime.datetime.now().month, 1)
@@ -75,7 +80,9 @@ def get_historical_intraday_data(parameters, startmonth, startyear, endmonth, en
         modified_date = datetime.datetime.fromtimestamp(historical_data_csv.stat().st_mtime).date()
         today_date = datetime.datetime.today().date()
         if modified_date == today_date:
-            final_data = pd.read_csv('historical_data.csv')
+            final_data = pd.read_csv('historical_data.csv',index_col=1)
+            final_data = final_data.drop(final_data.columns[0],1)
+            final_data.index = pd.to_datetime(final_data.index)
             exists = True
     if not exists:
         for period in date_range:
@@ -90,28 +97,39 @@ def get_historical_intraday_data(parameters, startmonth, startyear, endmonth, en
             net_data.extend(readings)
         final_data = pd.DataFrame(net_data, columns=header)
         final_data.to_csv('historical_data.csv')
-    return final_data.sort_values(by=['time'])
+    return final_data.sort_index()
 
 
 def main():
     counter = 0
     generic_indicatior_parameters = {'symbol': 'PLTR',
-                                     'interval': '15min',
+                                     'interval': '60min',
                                      'time_period': '60',
                                      'datatype': 'csv',
                                      'series_type': 'close',
                                      'apikey': '1A673HM6LF1CZG2I'
                                      }
-    get_historical_intraday_data(
-        parameters=generic_indicatior_parameters, startmonth=9,
+    historical_data = get_historical_intraday_data(
+        parameters=generic_indicatior_parameters, startmonth=10,
         startyear=2020, endmonth=5, endyear=2021,counter=counter)
-    time.sleep(65)
-    metric_dfs = get_indicator_metrics(
-        functions=["Earnings", "Income_Statement", "Balance_Sheet", "Cash_Flow", "SMA_High_Usage", "EMA_High_Usage",
-                   "T3",
-                   "STOCH_High_Usage", "RSI_High_Usage", "ADX_High_Usage", "CCI_High_Usage", "AROON_High_Usage",
-                   "BBANDS_High_Usage", "ULTOSC", "HT_TRENDLINE"], parameters=generic_indicatior_parameters, counter=counter)
-
-
+    extended_historical_data = add_all_ta_features(historical_data,open='open',close='close',high='high',low='low',volume='volume')
+    # metric_dfs = get_indicator_metrics(
+    #     functions=["Earnings", "Income_Statement", "Balance_Sheet", "Cash_Flow", "SMA_High_Usage", "EMA_High_Usage",
+    #                "T3",
+    #                "STOCH_High_Usage", "RSI_High_Usage", "ADX_High_Usage", "CCI_High_Usage", "AROON_High_Usage",
+    #                "BBANDS_High_Usage", "ULTOSC", "HT_TRENDLINE"], parameters=generic_indicatior_parameters, counter=counter)
+    extended_historical_data.to_csv('extended_historical_data.csv')
+    for i, col in enumerate(extended_historical_data.columns):
+        plt.figure(figsize=(10, 8), dpi=100)
+        extended_historical_data[col].plot(fig=plt.figure(i),x_compat=True)
+        plt.xlabel("Dates")
+        plt.setp(plt.gca().xaxis.get_majorticklabels(), rotation=90)
+        plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=10))
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%d-%m-%Y"))
+        plt.title(col)
+        fig = plt.gcf()
+        plt.tight_layout()
+        fig.savefig('plots/'+ str(i) + '_' + col + '.png', dpi=300)
+    plt.show()
 if __name__ == "__main__":
     main()
